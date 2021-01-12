4155701e1e9ef3e39681254cc8886536cb14bfca
Guillaume Briant <guillaume.briant@free.fr>
2021-01-12 18:54:04 +0100

    max game speed

--- Assets/Scripts/Environment/GameManager.cs
+++ Assets/Scripts/Environment/GameManager.cs
@@ -24,8 +24,13 @@ public class GameManager : MonoBehaviour
     private int dodgeMultiplier = 1;
     private int ovniMultiplier = 2;
     
-    private float upgradeDifficultyHolder;
+    //private float upgradeDifficultyHolder;
     private float currentTimeScale = 1;
+    private float runTimer = 0;
+    private float rawTimeScaleHolder = 1;
+
+    private bool startRunTimer = false;
+    private bool isRunTimerCounting = false;
 
     [HideInInspector]
     public bool isPochonInGame;
@@ -46,9 +51,11 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private float traveledDistance = 0;
     [SerializeField]
-    private float distanceUpgradeDifficulty;
+    private float timeToUpgradeDifficulty;
     [SerializeField]
     private float timescaleDifficulty;
+    [SerializeField]
+    private float maxSpeedTimeScale;
 
     [Header("Steps of bonus spawn and their spawn rates")]
     [SerializeField]
@@ -96,8 +103,6 @@ public class GameManager : MonoBehaviour
         inputManager = FindObjectOfType<InputManager>();
         menuManager = FindObjectOfType<MenuManager>();
 
-        upgradeDifficultyHolder = distanceUpgradeDifficulty;
-
         //if save file doesn't exist : create one with values = 0
         SaveSystem.InitiateDataFile();
     }
@@ -106,6 +111,7 @@ public class GameManager : MonoBehaviour
         if (gameState == GameState.PLAYING) {
             AddScore();
             DistanceUpdate();
+            DifficultyUpdate();
             BonusUpdate();
         }
     }
@@ -137,6 +143,7 @@ public class GameManager : MonoBehaviour
 
     public void StartPlayingInputs() {  //the player start playing
         inputManager.isRegisteringInputs = true;
+        startRunTimer = true;
 
         player.startCopFollowTimer = true;
         cop.CatchUpToPlayer();
@@ -146,6 +153,7 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 1;
         UpdatePlayerStats();
         gameState = GameState.FINISHED;
+        isRunTimerCounting = false;
 
         menuManager.InGameToLose();
     }
@@ -173,7 +181,11 @@ public class GameManager : MonoBehaviour
         score = 0;
         nbGoldDiscs = 0;
         traveledDistance = 0;
+        runTimer = 0;
+        startRunTimer = false;
+        isRunTimerCounting = false;
         Time.timeScale = 1;
+        rawTimeScaleHolder = 1;
     }
 
     private IEnumerator ReplayCorout() {
@@ -198,11 +210,11 @@ public class GameManager : MonoBehaviour
         //    Debug.Log("difficulty up");
         //}
 
-        if (traveledDistance >= distanceUpgradeDifficulty) {    //upgrade difficulty according to travaled distance
-            Time.timeScale += timescaleDifficulty;
-            distanceUpgradeDifficulty += upgradeDifficultyHolder;
-            Debug.Log("difficulty up");
-        }
+        //if (traveledDistance >= distanceUpgradeDifficulty) {    //upgrade difficulty according to travaled distance
+        //    Time.timeScale += timescaleDifficulty;
+        //    distanceUpgradeDifficulty += upgradeDifficultyHolder;
+        //    Debug.Log("difficulty up");
+        //}
 
         if (traveledDistance >= distanceStartSpawningTmax) {                    //last bonus spawn
             bonusSpawnRates.ChangeTmaxSpawnRate(TmaxSpawnRate_3);
@@ -219,6 +231,31 @@ public class GameManager : MonoBehaviour
             itemManager.EnableBonus();
             bonusSpawnRates.ChangePochonSpawnRate(pochonSpawnRate_1);
             bonusSpawnRates.ChangeClaquetteSpawnRate(claquetteSpawnRate_1);
+        }
+    }
+
+    private void DifficultyUpdate() {
+        if (startRunTimer) {
+            isRunTimerCounting = true;
+            runTimer = 0;
+
+            startRunTimer = false;
+        }
+
+        if (isRunTimerCounting) {
+            if (gameState != GameState.PAUSE && rawTimeScaleHolder < maxSpeedTimeScale) {
+                runTimer += Time.unscaledDeltaTime;
+
+                if (runTimer >= timeToUpgradeDifficulty) {
+                    Debug.Log("upgrade difficulty");
+                    rawTimeScaleHolder += timescaleDifficulty;
+                    Time.timeScale += timescaleDifficulty;
+                    runTimer = 0;
+                }
+
+                if (rawTimeScaleHolder >= maxSpeedTimeScale) Debug.Log("max speed reached");
+            }
+
         }
     }
 
