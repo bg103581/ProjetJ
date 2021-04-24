6f5d4192f19ba3614b923c5f3fc955d87c34e09b
Guillaume <guillaume.briant@free.fr>
2021-04-25 00:19:21 +0200

    canvas scale fix

--- Assets/Scripts/Ads/AdMobManager.cs
+++ Assets/Scripts/Ads/AdMobManager.cs
@@ -2,14 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using EasyMobile;
+using UnityEngine.SocialPlatforms;
+using TMPro;
 
 public class AdMobManager : MonoBehaviour
 {
     public static AdMobManager current;
 
+    [SerializeField] private TMP_Text userScoreText;
+
     private void Awake()
     {
         current = this;
+
+        if (!RuntimeManager.IsInitialized())
+        {
+            RuntimeManager.Init();
+        }
+    }
+
+    private void Start()
+    {
+        if (!GameServices.IsInitialized())
+        {
+            GameServices.Init();
+        }
     }
 
     public void ShowBannerAds()
@@ -30,6 +47,58 @@ public class AdMobManager : MonoBehaviour
         if (Advertising.IsRewardedAdReady())
         {
             Advertising.ShowRewardedAd();
+        }
+    }
+
+    public void ShowLeaderboard()
+    {
+        if (GameServices.IsInitialized())
+        {
+            GameServices.ShowLeaderboardUI();
+        }
+    }
+
+    public void ShowAchievement()
+    {
+        if (GameServices.IsInitialized())
+        {
+            GameServices.ShowAchievementsUI();
+        }
+    }
+
+    public void SubmitScoreToLeaderboard()
+    {
+        if (GameServices.IsInitialized())
+        {
+            GameServices.ReportScore(100, EM_GameServicesConstants.Leaderboard_OvniRacerLeaderboard);
+        }
+    }
+
+    public void LoadLocalUserScore()
+    {
+        if (GameServices.IsInitialized())
+        {
+            GameServices.LoadLocalUserScore(EM_GameServicesConstants.Leaderboard_OvniRacerLeaderboard, OnLocalUserScoreLoaded);
+        }
+    }
+
+    private void OnLocalUserScoreLoaded(string leaderboardName, IScore score)
+    {
+        if (score != null)
+        {
+            userScoreText.text = "Your score is : " + score.value;
+        }
+        else
+        {
+            userScoreText.text = "You don't have any score reported to leaderboard " + leaderboardName;
+        }
+    }
+
+    public void UnlockAchievement()
+    {
+        if (GameServices.IsInitialized())
+        {
+            GameServices.UnlockAchievement(EM_GameServicesConstants.Achievement_testAchievements);
         }
     }
 }
